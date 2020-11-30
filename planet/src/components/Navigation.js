f7a56f2666ffa2c3b840384bbe08b1c3f3413530
dylan <dylanabozarth@gmail.com>
2020-11-30 09:45:26 -0800

    adding bootstrap

--- planet/src/components/Navigation.js
+++ planet/src/components/Navigation.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Navigation = () => (
-  <div>
+  <div className="text-center">
     <ul>
       <li><Link to="/">Home</Link></li>
       <li><Link to="/about">A shiba</Link></li>
--- planet/src/routes/Home.js
+++ planet/src/routes/Home.js
@@ -2,15 +2,18 @@ import React, { Fragment } from "react";
 
 const Home = () => (
   <Fragment>
-    <div className="text-center">
-      <h1 className="text-center">LETS DO WEBPACK YEAHAHAHA</h1>
-      <div className="planet">
-        <div className="wrap">
-          <div className="background"></div>
-          <div className="clouds"></div>
+    <div className="container-fluid homepage">
+      <div className="row">
+        <h1 className="text-center">LETS DO WEBPACK YEAHAHAHA</h1>
+        <div className="planet">
+          <div className="wrap">
+            <div className="background"></div>
+            <div className="clouds"></div>
+          </div>
+          <div className="mask"></div>
         </div>
-        <div className="mask"></div>
       </div>
+      
     </div>
   </Fragment>
 );
