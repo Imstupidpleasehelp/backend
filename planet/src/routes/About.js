8921381dd2d9b0b6ce426941b6e33e8a74bd9b6f
dylan <dylanabozarth@gmail.com>
2020-11-29 20:52:30 -0800

    new stuff

---
+++ planet/src/routes/About.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react'
+import dog from '../assets/images/shiba.png'
+
+const About = () => (
+  <Fragment>
+   <img src={dog} className="small-img"/>
+  </Fragment>
+)
+
+export default About
---
+++ planet/src/routes/Home.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from "react";
+
+const Home = () => (
+  <Fragment>
+    <div className="text-center">
+      <h1>LETS DO WEBPACK YEAHAHAHA</h1>
+      <div className="planet">
+        <div className="wrap">
+          <div className="background"></div>
+          <div className="clouds"></div>
+        </div>
+        <div className="mask"></div>
+      </div>
+    </div>
+  </Fragment>
+);
+
+export default Home;
